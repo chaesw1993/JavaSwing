46012d8b372c7027f31215fe1c2e96bd4d3898c7
평일반 <@itbank-PC>
2015-07-30 15:20:49 +0900

    2015.07.30(Swing, Lotto 예제의 뼈대)

---
+++ JavaSwing/src/swing/LottoService.java
@@ -0,0 +1,9 @@
+package swing;
+/*
+ * @ Date : 2015.07.30
+ * @ Author : Chae S W
+ * @ Story : 로또(Swing) 예제
+ */
+public class LottoService {
+
+}
---
+++ JavaSwing/src/swing/LottoUI.java
@@ -0,0 +1,23 @@
+package swing;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+/*
+ * @ Date : 2015.07.30
+ * @ Author : Chae S W
+ * @ Story : 로또(Swing) 예제
+ */
+public class LottoUI extends JFrame implements ActionListener{
+	// UI : User Interface - 사용자화면
+	public LottoUI() {
+		
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+	}
+}
